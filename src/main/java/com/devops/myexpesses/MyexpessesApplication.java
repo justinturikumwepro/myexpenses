c8dd05387bdfaa272806d312950bcd3aa3942eb8
justinturikumwe <148125759+justinturikumwe@users.noreply.github.com>
2025-06-13 23:16:24 -0400

    initial commit

---
+++ src/main/java/com/devops/myexpesses/MyexpessesApplication.java
@@ -0,0 +1,13 @@
+package com.devops.myexpesses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyexpessesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyexpessesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/myexpesses/MyexpessesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.myexpesses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyexpessesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
